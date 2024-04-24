ef6bc5a26ef60542da6ca795b33c956ef9465b81
Francesca Barletta <wallebar@gmail.com>
2024-04-24 07:15:32 +0200

    add appfooter

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -3,9 +3,121 @@
 </script>
 
 <template>
-<h2>FOOTER</h2>
+<div class="section">
+    <div class="bg-overlay">
+        <div class="container">
+            <div class="row footermenu">
+                <img class="logo" src="/img/menulogo.png" alt="">
+                <ul class="menu">
+                    <li><a href="#">HOME</a></li>
+                    <li><a href="#">PAGES</a></li>
+                    <li><a href="#">TOURNAMENT</a></li>
+                    <li><a href="#">SHOP</a></li>
+                    <li><a href="#">BLOG</a></li>
+                    <li><a href="#">CONTACT</a></li>
+                </ul>
+            </div>
+            <hr>
+            <div class="flex">
+                <div>
+                    <span class="white">Copyright RaxG | Designed by</span>
+                    <span class="green">NextGenerationDev</span>
+                </div>
+            
+                <ul class="social">
+                        <li class="round"><a href="#"><img src="/img/image(4).svg" alt=""></a></li>
+                        <li class="round"><a href="#"><img src="/img/image(5).svg" alt=""></a></li>
+                        <li class="round"><a href="#"><img src="/img/image(6).svg" alt=""></a></li>
+                        <li class="round"><a href="#"><img src="/img/image(7).svg" alt=""></a></li>
+                </ul>
+            </div>
+        </div>
+
+    </div>
+</div>
 </template>
 
-<style lang="scss">
-
+<style lang="scss"scoped>
+.section{
+    background-image:url(/img/footer-bg.png);
+    background-size: cover;
+    .bg-overlay{
+        background: rgba(25, 25, 53, 0.7);
+    }
+    
+    
+}
+.container{
+    width: 80%;
+    margin: 0 auto;
+    height: 370px;
+    padding-top: 150px;
+    hr{
+        color: rgba(255, 255, 255, 0.4);
+        margin-bottom: 16px;
+    }
+}
+.row{
+    display:flex;
+    align-items: center;
+    margin-bottom: 22px;
+}
+.footermenu{
+    height: 100px;
+    justify-content: space-between;
+}
+.menu{
+    display: flex;
+    align-items: center;
+    color: white;
+    gap: 27px;
+    li{
+        &:hover{
+            color:#05CC7C;
+        }
+    }
+    img{
+        width:10px;
+        margin-left:1px;
+        transform: translatey(10%);
+        &:hover{
+            fill:#05CC7C;
+        }
+    }
+}
+.logo{
+   height: 70px;
+   width: 196px;
+   margin-top: 10px;
+}
+.green{
+    color: #05CC7C;
+}
+.white{
+    color:white;
+}
+.flex{
+    display:flex;
+    justify-content: space-between;
+    align-items: center;
+}
+.social{
+    display: flex;
+    gap:20px;
+    .round{
+        width:50px;
+        aspect-ratio: 1/1;
+        border-radius: 9999px;
+        display:flex;
+        align-items: center;
+        justify-content: center;
+        background-color: #202046;
+        margin-bottom:12px;
+    img{
+        filter: invert(100%);
+        width: 20px;
+        transform: translateY(2px);
+    }
+    }
+}
 </style>
